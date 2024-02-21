5027b34ea40116d8f22e49f155faef4416a7bb82
rf19990202 <heyu880202@gmail.com>
2024-02-21 15:50:23 +0800

    Add MemberController

---
+++ CreatorsPlatform/Controllers/MemberController.cs
@@ -0,0 +1,29 @@
+using CreatorsPlatform.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CreatorsPlatform.Controllers
+{
+    public class MemberController : Controller
+    {
+        private readonly ILogger<MemberController> _logger;
+
+        public MemberController(ILogger<MemberController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Signup()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
