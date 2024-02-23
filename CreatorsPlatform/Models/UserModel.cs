84bdd3d0f69810f836f164665c1e7f2eae4691fb
rf19990202 <heyu880202@gmail.com>
2024-02-23 16:16:16 +0800

    Add user model

--- CreatorsPlatform/Models/UserModel.cs
+++ CreatorsPlatform/Models/UserModel.cs
@@ -1,4 +1,20 @@
-namespace CreatorsPlatform.Models{
-    public class User{
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CreatorsPlatform.Models
+{
+    // [PrimaryKey(nameof(ID))]
+    internal class User
+    {
+        [Key]
+        public int ID { get; set; }
+        [Column(TypeName = "nvarchar(40)")]
+        public string? Name { get; set; }
+        [Required]
+        public required string Email { get; set; }
+        [Required]
+        public required string Password { get; set; }
+        [NotMapped]
+        public IList<User> OneUserLoaded { get; set; }
     }
 }
