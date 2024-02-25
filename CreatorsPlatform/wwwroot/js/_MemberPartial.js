7578ad54265adb52c4228a82e20fa2828859086b
rf19990202 <heyu880202@gmail.com>
2024-02-25 12:13:02 +0800

    Add _MemberPartial RegisterForm LoginForm

--- CreatorsPlatform/wwwroot/js/_MemberPartial.js
+++ CreatorsPlatform/wwwroot/js/_MemberPartial.js
@@ -9,11 +9,35 @@ class loginform {
 
 // document.getElementById("switchBtn").appendChild(loginForm);
 
-const test = new loginform();
-window.onload = () => {
-    console.log(document.querySelector('#switchBtns'));
-    document.querySelector('#switchBtns').addEventListener('click', () => {
-        document.getElementById("entrance").appendChild(test.ele);
-    });
-}
+//const test = new loginform().ele;
+//window.onload = () => {
+//    console.log(document.querySelector('#switchBtns'));
+//    document.querySelector('#switchBtns').addEventListener('mouseover', () => {
+//        document.getElementById("entrance").appendChild(test.ele);
+//    });
+//}
 
+$("#member").mouseenter(function () {
+    $("#entrance").slideDown();
+});
+$("#container").mouseleave(function () {
+    $("#entrance").slideUp();
+});
+
+
+$("#loginBtn").mouseenter(function () {
+    $("#LoginForm").slideDown();
+    $("#RegisterForm").slideUp();
+});
+$("#container").mouseleave(function () {
+    $("#LoginForm").slideUp();
+});
+
+
+$("#RegisterBtn").mouseenter(function () {
+    $("#RegisterForm").slideDown();
+    $("#LoginForm").slideUp();
+});
+$("#container").mouseleave(function () {
+    $("#RegisterForm").slideUp();
+});
