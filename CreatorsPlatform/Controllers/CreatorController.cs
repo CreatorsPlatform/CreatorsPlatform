d9c1d40ed8ff04002f9bdd248fb192d019ed31a2
WarotaWorks <warotasandesu@gmail.com>
2024-03-01 21:43:26 +0800

    Add CreatorController, add it's index, and a part of body.

---
+++ CreatorsPlatform/Controllers/CreatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CreatorsPlatform.Controllers
+{
+    public class CreatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
