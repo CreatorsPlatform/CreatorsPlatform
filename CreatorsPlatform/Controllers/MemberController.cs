91cb51a0ea15b71fac09064567b6dffdbe52ebad
rf19990202 <heyu880202@gmail.com>
2024-03-05 16:13:12 +0800

    加入部分sql

--- CreatorsPlatform/Controllers/MemberController.cs
+++ CreatorsPlatform/Controllers/MemberController.cs
@@ -1,4 +1,4 @@
-using CreatorsPlatform.Models;
+﻿using CreatorsPlatform.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,6 +6,11 @@ namespace CreatorsPlatform.Controllers
 {
     public class MemberController : Controller
     {
+        private static string 使用者哀低 = "UserId";
+        private static string 使用者信箱 = "Email";
+        private static string 使用者密碼 = "Password";
+        private string 用來檢查重複的使用者的SQL指令 = $"SELECT {使用者信箱} FROM User WHERE UserEmail = @UserEmail;";
+        private string 用來檢查輸入的電子信箱與密碼是否符合存在資料庫的電子信箱與密碼的SQL指令 = $"SELECT {使用者信箱}, {使用者密碼} FROM User WHERE {使用者信箱} = @A AND {使用者密碼} =@B;";
         public IActionResult Index()
         {
             if (2 == 1)
