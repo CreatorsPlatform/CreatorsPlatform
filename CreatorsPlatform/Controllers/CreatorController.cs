2b5af4e1adff1e444dd1aec01a9af306e4d25459
WarotaWorks <warotasandesu@gmail.com>
2024-03-05 11:07:16 +0800

    創作者Controller添加數個View=>建立貼文、接受委託表單、訂閱方案

--- CreatorsPlatform/Controllers/CreatorController.cs
+++ CreatorsPlatform/Controllers/CreatorController.cs
@@ -4,12 +4,26 @@ namespace CreatorsPlatform.Controllers
 {
     public class CreatorController : Controller
     {
+        // 創作者首頁
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Post()
+        // 創作者建立貼文(修改位置待訂)
+        public IActionResult CreatePost()
+        {
+            return View();
+        }
+
+        // 創作者建立接受委託表單(修改位置待訂)
+        public IActionResult CreateCommisionForm()
+        {
+            return View();
+        }
+
+        // 創作者建立訂閱方案(修改位置待訂)
+        public IActionResult CreateSubcriptionPlans()
         {
             return View();
         }
