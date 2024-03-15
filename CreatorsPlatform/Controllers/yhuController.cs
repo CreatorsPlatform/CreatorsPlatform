34dacc8e91a52da3e038ca993575de0107abf187
royam9 <lolm19070@gmail.com>
2024-03-15 21:27:35 +0800

    yhu部分for rt

--- CreatorsPlatform/Controllers/yhuController.cs
+++ CreatorsPlatform/Controllers/yhuController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.EntityFrameworkCore.Diagnostics;
 using System.Collections.Generic;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -40,16 +39,16 @@ namespace CreatorsPlatform.Controllers
                     new Dftest { Nickname = "Jennifer Anderson2tay", ImageURL = "https://example.com/image9.jpg", Title = "Excepteur sint occaecat cupidatat non proident", Description = "Sunt in culpa qui officia deserunt mollit anim id est laborum." }
                 };
         //LINQ方法(TEST)
-         public List<Dftest> newsListLinq(int x , int y, List<Dftest> ListName)
+        public List<Dftest> newsListLinq(int x, int y, List<Dftest> ListName)
         {
-            if(ListName.Count< y)
+            if (ListName.Count < y)
             {
                 y = ListName.Count;
             }
-            List<Dftest> Form1= new List<Dftest>();
+            List<Dftest> Form1 = new List<Dftest>();
             for (int i = x; i < y; i++)
-            {   
-                    Form1.Add(ListName[i]);
+            {
+                Form1.Add(ListName[i]);
             }
             return Form1;
         }
@@ -58,33 +57,46 @@ namespace CreatorsPlatform.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult PersonalUser(int _CurrentMsg,string tapy )
+        public ActionResult PersonalUser(int _CurrentMsg, string tapy)
         {
             List<Dftest> updateList = new List<Dftest>() { };
             switch (tapy)
             {
                 case "newmsg":
-                    updateList = newsListLinq(_CurrentMsg,_CurrentMsg+10, newmsg);
+                    updateList = newsListLinq(_CurrentMsg, _CurrentMsg + 10, newmsg);
                     break;
                 case "subscribemsg":
-                    updateList = newsListLinq(_CurrentMsg,_CurrentMsg + 10, subscribemsg);
+                    updateList = newsListLinq(_CurrentMsg, _CurrentMsg + 10, subscribemsg);
                     break;
-            }           
+            }
             return Json(updateList);
         }
         public IActionResult IMAGINK()
         {
             return View();
         }
-		public IActionResult Payment()
-		{
-			return View();
-		}
-		public IActionResult EntrustPayment()
-		{
-			return View();
-		}
-	}
-    
+        public IActionResult Payment()
+        {
+            return View();
+        }
+        public IActionResult EntrustPayment()
+        {
+            return View();
+        }
+        public IActionResult Individual()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+    }
+
 
 }
